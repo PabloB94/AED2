7df79bf2b0c5361004b57db07523fb308c653518
PabloB94 <p.beltran@alumnos.upm.es>
2017-11-25 18:28:16 +0100

    Constructor test succeeded
    Encode and decode are not functional

--- src/aed/huffman/Huffman.java
+++ src/aed/huffman/Huffman.java
@@ -27,13 +27,19 @@ public class Huffman {
     		Character ch = paths[i].getCh();
     		char[] code = paths[i].getCode().toCharArray();
     		Position<Character> pos = this.huffmanTree.root();
-    		for(int j = 0; j < code.length; j++){
+    		for(int j = 0; j < code.length - 1; j++){
     			if(code [j] == '0'){
-    				if(!this.huffmanTree.hasLeft(pos)) this.huffmanTree.insertLeft(pos, ch);
-    			}else if(code[j] == '1'){
+    				if(!this.huffmanTree.hasLeft(pos)) this.huffmanTree.insertLeft(pos, ' ');
+    				pos = this.huffmanTree.left(pos);
+    			}else{
     				if(!this.huffmanTree.hasRight(pos)) this.huffmanTree.insertRight(pos, ' ');
     				pos = this.huffmanTree.right(pos);
     			}
+    		}		
+    		if(code[code.length - 1] == '0'){
+    			if(!this.huffmanTree.hasLeft(pos)) this.huffmanTree.insertLeft(pos, ch);
+    		}else{
+    			if(!this.huffmanTree.hasRight(pos)) this.huffmanTree.insertRight(pos, ch);
     		}
     	}
     }
@@ -46,20 +52,32 @@ public class Huffman {
      * containing only characters '0' and '1'.
      */
     public String encode(String text) {
-	// CAMBIA
-	// llama el metodo findCharacterCode
-	return "";
+		char[] toEncode = text.toCharArray();
+		String compressed = new String("");
+		for(int i = 0; i < toEncode.length; i++){
+			String code = findCharacterCode(toEncode[i], this.huffmanTree.root());
+			if (code != null) compressed = compressed + code;
+		}
+		return compressed;
     }
 
-    // CAMBIA e UTILIZA si quiereis
-    private String findCharacterCode(Character ch, 
-			     BinaryTree<Character> tree,
-			     Position<Character> pos,
-			     String path) {
-
-	// Return null if character not found, and otherwise the
-	// string of "0" and "1" that lead to the node with the character
-        return null;			     
+    
+    private String findCharacterCode(Character ch, Position<Character> pos) {
+    	if(pos.element() != ch){
+    		if(!this.huffmanTree.hasLeft(pos) && !this.huffmanTree.hasRight(pos)) return null;
+    		if(this.huffmanTree.left(pos).element() == ch) return "0";
+    		if(this.huffmanTree.right(pos).element() == ch) return "1";
+    		String res;
+    		if(this.huffmanTree.left(pos).element() == ' '){
+    			res = findCharacterCode(ch, this.huffmanTree.left(pos));
+    			if(res == null) return null;
+    			return "0" + res;
+    		}else{
+    			res = findCharacterCode(ch, this.huffmanTree.right(pos));
+    			if(res == null) return null;
+    			return "1" + res;
+    		}   		
+    	}else return "";   				     
     }
 
 
@@ -70,8 +88,21 @@ public class Huffman {
      * '0' and '1's), returns the corresponding normal text.
      */
     public String decode(String encodedText) {
-	// CAMBIA
-	return "";
+		char[] toDecode = encodedText.toCharArray();
+		String text = new String("");
+		Position<Character> pos = this.huffmanTree.root();
+		for(char a : toDecode){
+			if(a == '0'){
+				text = text + this.huffmanTree.left(pos).element();
+				pos = this.huffmanTree.root();
+			}else if(pos.element() != ' '){
+				text = text + pos.element();
+				pos = this.huffmanTree.root();
+			}else{
+				pos = this.huffmanTree.right(pos);
+			}
+		}
+		return text;
     }
 }
 
