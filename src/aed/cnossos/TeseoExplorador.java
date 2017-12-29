bed398df660a0abe134623ebdb984dfd59ecb3e2
PabloB94 <p.beltran@alumnos.upm.es>
2017-12-29 18:45:44 +0100

    Individual Extra Añadido
    Test pasados correctamente en local. Sintaxis de función forEach de la
    clase Iterable no reconocida por el tester de la asignatura.

---
+++ src/aed/cnossos/TeseoExplorador.java
@@ -0,0 +1,56 @@
+package aed.cnossos;
+
+import es.upm.aedlib.Pair;
+import es.upm.aedlib.positionlist.*;
+import es.upm.aedlib.Position;
+import java.util.Hashtable;
+
+public class TeseoExplorador {
+	
+	public static Pair<Object,PositionList<PuntoCardinal>> explora(Laberinto cnossos) {
+		PositionList<PuntoCardinal> path = new NodePositionList<PuntoCardinal>();
+		Hashtable<String, PositionList<PuntoCardinal>> table = new Hashtable<String, PositionList<PuntoCardinal>>();		
+		return search(cnossos, table, path);		
+	}
+	
+	private static Pair<Object,PositionList<PuntoCardinal>> search(Laberinto cnossos, 
+			Hashtable<String, PositionList<PuntoCardinal>> table, PositionList<PuntoCardinal> path){
+		if(cnossos.tieneTesoro()) return new Pair<Object, PositionList<PuntoCardinal>>(cnossos.getTesoro(), path);
+		if(!cnossos.sueloMarcadoConTiza()) cnossos.marcaSueloConTiza();
+		PositionList<PuntoCardinal> toExplore;
+		toExplore = table.get(cnossos.printLocation());
+		if (toExplore == null){
+			PositionList<PuntoCardinal> aux = new NodePositionList<PuntoCardinal>();
+			Iterable<PuntoCardinal> caminos = cnossos.caminos();
+			caminos.forEach(item -> aux.addLast(item));
+			toExplore = aux;
+		}
+		backtrack(toExplore, path);
+		if(toExplore.isEmpty()){
+			if(path.isEmpty()) return null;
+			cnossos.ir(PuntoCardinal.opuesto(path.remove(path.last())));
+		}else{
+			PuntoCardinal next = toExplore.remove(toExplore.first());
+			table.put(cnossos.printLocation(), toExplore);
+			path.addLast(next);
+			cnossos.ir(next);
+		}	
+		return search(cnossos, table, path);
+	}
+	
+	private static void backtrack(PositionList<PuntoCardinal> toExplore, PositionList<PuntoCardinal> path){
+		if(path.isEmpty() || toExplore.isEmpty()) return;
+		Position<PuntoCardinal> pc = toExplore.first();
+		PuntoCardinal last = path.last().element();
+		for(int i = 0; i < toExplore.size(); i++){
+			if(pc.element() == PuntoCardinal.opuesto(last)){
+				toExplore.remove(pc);
+				return;
+			}
+			pc = toExplore.next(pc);
+		}
+	}	
+}
+
+
+
---
+++ src/aed/compress/AttachableLinkedBinaryTree.java
@@ -0,0 +1,29 @@
+package aed.compress;
+
+import es.upm.aedlib.Position;
+import es.upm.aedlib.tree.*;
+
+
+public class AttachableLinkedBinaryTree<E> extends LinkedBinaryTree<E> implements AttachableBinaryTree<E> {
+
+	public void attach(Position<E> pos, BinaryTree<E> leftTree, BinaryTree<E> rightTree) {
+		Position<E> rootLeft = leftTree.root();
+		Position<E> rootRight = rightTree.root();
+		this.insertLeft(pos, rootLeft.element());
+		this.insertRight(pos, rootRight.element());
+		if(leftTree.hasLeft(rootLeft) || leftTree.hasRight(rootLeft)) clone(this.left(pos), leftTree, rootLeft);
+		if(rightTree.hasLeft(rootRight) || rightTree.hasRight(rootRight)) clone(this.right(pos), rightTree, rootRight);		
+	}
+	
+	private void clone(Position<E> pos, BinaryTree<E> tree, Position<E> root){
+		if(tree.hasLeft(root)){
+			this.insertLeft(pos, tree.left(root).element());
+			clone(this.left(pos), tree, tree.left(root));
+		}
+		if(tree.hasRight(root)){
+			this.insertRight(pos, tree.right(root).element());
+			clone(this.right(pos), tree, tree.right(root));
+		}		
+	}
+
+}
